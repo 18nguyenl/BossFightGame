a325af136016f35dbd8260c429bb2cf1cc69dfc2
Eldrojin <dystraxus@gmail.com>
2017-08-24 11:50:16 -0500

    Add Basic Animation and Platform
    Add new Idle animation and platform and start scripting

---
+++ Assets/Scripts/moveRight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveRight : MonoBehaviour {
+    Rigidbody2D movement;
+    Animator playerAnimations;
+
+	// Use this for initialization
+	void Start () {
+        movement = gameObject.GetComponent<Rigidbody2D>();
+        playerAnimations = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetAxis("Horizontal") > 0) 
+        {
+            movement.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0));
+        }
+		
+	}
+}
