6d93c5d5398ded15df227903ecd8d3d0f002ab7c
Eldrojin <dystraxus@gmail.com>
2017-08-25 11:47:58 -0500

    Add Movement and Basic Animation
    Gotta get the ground jump rules to work!

--- Assets/Scripts/moveRight.cs
+++ Assets/Scripts/moveRight.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class moveRight : MonoBehaviour {
     Rigidbody2D movement;
     Animator playerAnimations;
+    CapsuleCollider2D skeleton;
 
 	// Use this for initialization
 	void Start () {
         movement = gameObject.GetComponent<Rigidbody2D>();
         playerAnimations = gameObject.GetComponent<Animator>();
+        skeleton = gameObject.GetComponent<CapsuleCollider2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        if(Input.GetAxis("Horizontal") > 0) 
+        playerAnimations.SetFloat("HorizontalVal", movement.velocity.x);
+        Debug.Log("not moving");
+        if(Input.GetKey(KeyCode.D)  || Input.GetKeyDown(KeyCode.D) && skeleton.) 
         {
-            movement.AddForce(new Vector2(Input.GetAxis("Horizontal"), 0));
+            Debug.Log("moving Right");
+            movement.velocity = new Vector2(3.0f, 10.0f);
+            playerAnimations.SetFloat("HorizontalVal", movement.velocity.x);            
         }
 		
 	}
