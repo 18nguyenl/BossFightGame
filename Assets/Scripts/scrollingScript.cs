fc2585812fd16fb4349972d08f70039efd999377
Eldrojin <dystraxus@gmail.com>
2017-08-29 11:50:27 -0500

    Start Adding Parallax Scrolling
    the backgrounds flips weirdly unfortunately.

---
+++ Assets/Scripts/scrollingScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingScript : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.Space)) {
+			ScrollLeft();
+		} else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.Space)) {
+			ScrollRight();
+		}
+	}
+	
+	void ScrollLeft() {
+		transform.GetChild(0).position = new Vector2(-Camera.main.transform.position.x  - 2f, 0);
+		transform.GetChild(1).position = new Vector2(-Camera.main.transform.position.x, 0);
+	}
+
+	void ScrollRight() {
+		transform.GetChild(0).position = new Vector2(Camera.main.transform.position.x  + 2f, 0);
+		transform.GetChild(1).position = new Vector2(Camera.main.transform.position.x, 0);
+	}
+}
